fd21cf65284170dc8b1652e8d1e67523a8a8228a
KKonaOG <38236274+KKonaOG@users.noreply.github.com>
2025-08-02 11:29:27 -0500

    Update download-plugins.js

--- scripts/download-plugins.js
+++ scripts/download-plugins.js
@@ -12,9 +12,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-
-#!/usr/bin/env node
-
 const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
